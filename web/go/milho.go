1cb30fc788a829ec4a7a45bef751235587fa3232
danfragoso <danilo.fragoso@gmail.com>
2021-03-29 19:32:23 -0300

    Add AST to web repl

--- web/go/milho.go
+++ web/go/milho.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"syscall/js"
 
 	"github.com/danfragoso/milho"
+	"github.com/danfragoso/milho/parser"
+	"github.com/danfragoso/milho/tokenizer"
 )
 
 func eval(this js.Value, inputs []js.Value) interface{} {
@@ -11,8 +14,24 @@ func eval(this js.Value, inputs []js.Value) interface{} {
 	return milho.Run(milhoSrc)
 }
 
+func ast(this js.Value, inputs []js.Value) interface{} {
+	milhoSrc := inputs[0].String()
+	tokens, err := tokenizer.Tokenize(milhoSrc)
+	if err != nil {
+		return fmt.Sprintf("+- Nil:[%s]\n", err)
+	}
+
+	ast, err := parser.Parse(tokens)
+	if err != nil {
+		return fmt.Sprintf("+- Nil:[%s]\n", err)
+	}
+
+	return ast.String()
+}
+
 func main() {
 	c := make(chan bool)
 	js.Global().Set("eval", js.FuncOf(eval))
+	js.Global().Set("ast", js.FuncOf(ast))
 	<-c
 }
