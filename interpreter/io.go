6f29e9eeabdf4ff6e6422a03092f521b7aa3596a
danfragoso <danilo.fragoso@gmail.com>
2021-04-22 00:43:36 -0300

    Fix pr bug

--- interpreter/io.go
+++ interpreter/io.go
@@ -2,16 +2,36 @@ package interpreter
 
 import "fmt"
 
+func resolveExpression(expr Expression, session *Session) (Expression, error) {
+	if expr.Type() == SymbolExpr {
+		return session.FindObject(expr.(*SymbolExpression).Identifier)
+	}
+
+	return expr, nil
+}
+
 func __pr(params []Expression, session *Session) (Expression, error) {
+	var err error
 	for _, param := range params {
-		fmt.Print(param.Value())
+		param, err = resolveExpression(param, session)
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Print(param)
 	}
 
 	return createNilExpression()
 }
 
 func __prn(params []Expression, session *Session) (Expression, error) {
+	var err error
 	for _, param := range params {
+		param, err = resolveExpression(param, session)
+		if err != nil {
+			return nil, err
+		}
+
 		fmt.Print(param.Value())
 	}
 
